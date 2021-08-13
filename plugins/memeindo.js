aae283dc1b92b798902ce1783b91a98f5804e5a8
ZyxMapLe <87278112+ZyxMapLe@users.noreply.github.com>
2021-08-14 05:01:38 +0700

    Update memeindo.js

--- plugins/memeindo.js
+++ plugins/memeindo.js
@@ -1,10 +1,10 @@
 let fetch = require("node-fetch")
 let handler = async (m, { conn }) => {
   let res = await fetch(global.API('https://leyscoders-api.herokuapp.com', '/api/memeindo?apikey=dappakntlll'))
-  if (!res.ok) throw await res.result()
+  if (!res.ok) throw await res.image()
   let json = await res.json()
-  if (!json.result) throw 'Err!'
-  conn.sendFile(m.chat, json.result, 'memeindo.png', json.image, m)
+  if (!json.image) throw 'Err!'
+  conn.sendFile(m.chat, json.image, 'memeindo.png', json.text, m)
 }
 
 handler.help = ['memeindo']
