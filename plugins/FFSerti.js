87bb1554e1858c41adaa2eda3b745e28acfa872b
ZyxMapLe <87278112+ZyxMapLe@users.noreply.github.com>
2021-08-14 03:51:04 +0700

    Update FFSerti.js

--- plugins/FFSerti.js
+++ plugins/FFSerti.js
@@ -1,14 +1,28 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-   response = args.join(' ')
-  if (!args) throw 'Masukkan Parameter'
-  m.reply('Sedang Diproses...')
-  let res = `http://rzky.net/docs/api/FFSerti/img.php?nama=${response}`
-  conn.sendFile(m.chat, res, 'FFSerti.jpg', `Nih kak`, m, false)
-}
-handler.help = ['ffserti'].map(v => v + ' <teks>')
-handler.tags = ['sticker']
-
-handler.command = /^(ffserti)$/i
-
-module.exports = handler
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, args }) => {
+
+   response = args.join(' ')
+
+  if (!args) throw 'Masukkan Parameter'
+
+  m.reply('Sedang Diproses...')
+
+  let res = `https://sertiojanganzapi.nasihosting.com/serti/serti1/img.php?nama=${response}`
+
+  conn.sendFile(m.chat, res, 'FFSerti.jpg', `Nih kak`, m, false)
+
+}
+
+handler.help = ['ffserti'].map(v => v + ' <teks>')
+
+handler.tags = ['sticker']
+
+
+
+handler.command = /^(ffserti)$/i
+
+
+
+module.exports = handler
+
